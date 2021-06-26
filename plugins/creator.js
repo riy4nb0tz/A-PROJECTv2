eba795c5b8402b14b9d69ecdc6daba3bff01466f
riy4nb0tz <86124000+riy4nb0tz@users.noreply.github.com>
2021-06-26 19:03:11 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6288294052009', 'MANK ARDI', m)
-  conn.fakeReply(m.chat, '_*TUH OWNER KU TOD*_', '0@s.whatsapp.net', '_OWNER GANZ_', 'status@broadcast')
+  this.sendContact(m.chat, '6283153406515', 'RIYAN BOTZ', m)
+  conn.fakeReply(m.chat, '_*TUH OWNER KU TOD*_', '0@s.whatsapp.net', '_MAU DI APAIN_', 'status@broadcast')
 }
 handler.command = /^owner$/i
 
